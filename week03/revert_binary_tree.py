3190e494e576ea15f70088092b43f452d39f4742
hksuki <kurumisuki5566@gmail.com>
2021-04-13 08:01:21 +0800

    add something

---
+++ week03/revert_binary_tree.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def invertTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+        if not root:
+            return None
+        left = root.left
+        right = root.right
+        root.left = self.invertTree(right)
+        root.right = self.invertTree(left)
+        return root
